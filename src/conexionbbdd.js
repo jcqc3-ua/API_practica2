27c2d481f3fab274810c5507c0e5445809981e6e
joan <joanclimentq@gmail.com>
2025-01-15 10:48:25 +0100

    Comment out alternative database connection configuration in conexionbbdd.js

--- src/conexionbbdd.js
+++ src/conexionbbdd.js
@@ -6,3 +6,10 @@ export var conexionbbdd = mysql.createConnection({
   password: 'machotes123',
   database: 'practica2iw',
 });
+
+/*export var conexionbbdd = mysql.createConnection({
+  host: 'sql206.infinityfree.com',
+  user: 'if0_38100605',
+  password: 'U9aY1SbQqN',
+  database: 'if0_38100605_iw_p2'});
+*/
